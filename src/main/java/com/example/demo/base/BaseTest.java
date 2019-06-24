42355d8c82d7cc0fb815b17431d52708551bf7de
liuhuan5546 <397431418@qq.com>
2019-06-24 11:29:17 +0800

    edit .gitignore

--- src/main/java/com/example/demo/base/BaseTest.java
+++ src/main/java/com/example/demo/base/BaseTest.java
@@ -1,9 +1,6 @@
 package com.example.demo.base;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 /**
  * @author 作者 xcc:
@@ -15,11 +12,11 @@ public class BaseTest {
     public static void main(String[]  args){
         System.out.print(reverseString("hello")+"\n");
         System.out.print(reverse("hello my name is travis")+"\n");
-        int j = (9) >>> 1;
-        System.out.print(j+"\n");
+        iteratorHashMap();
 
     }
 
+    //字符串倒序
     public static String reverseString(String value){
         List<String> a = Arrays.asList(value.split(""));
         Collections.reverse(a);
@@ -29,8 +26,49 @@ public class BaseTest {
         }
         return b;
     }
+    //字符串倒序
     public static String reverse(String str){
         return new StringBuilder(str).reverse().toString();
     }
 
+    /**
+     * 遍历HashMap的四种方法
+     */
+    public static void iteratorHashMap(){
+        Map<String,String> map = new HashMap();
+        map.put("one","临沂");
+        map.put("two","上海");
+        map.put("three","南昌");
+        map.put("four","江西");
+
+        //第一种：普通使用，二次取值
+        System.out.println("\n通过Map.keySet遍历key和value：");
+        for(String key:map.keySet())
+        {
+            System.out.println("Key: "+key+" Value: "+map.get(key));
+        }
+
+        //第二种
+        System.out.println("\n通过Map.entrySet使用iterator遍历key和value: ");
+        Iterator map1it=map.entrySet().iterator();
+        while(map1it.hasNext())
+        {
+            Map.Entry<String, String> entry=(Map.Entry<String, String>) map1it.next();
+            System.out.println("Key: "+entry.getKey()+" Value: "+entry.getValue());
+        }
+
+        //第三种：推荐，尤其是容量大时
+        System.out.println("\n通过Map.entrySet遍历key和value");
+        for(Map.Entry<String,String> entry: map.entrySet())
+        {
+            System.out.println("Key: "+ entry.getKey()+ " Value: "+entry.getValue());
+        }
+
+        //第四种
+        System.out.println("\n通过Map.values()遍历所有的value，但不能遍历key");
+        for(String v:map.values())
+        {
+            System.out.println("The value is "+v);
+        }
+    }
 }
